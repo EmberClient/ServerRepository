c049d10a9cb55f7803b7037c08c3cf40a88e7350
amplitudes <hey@amplitudes.me>
2022-12-04 22:19:14 +0000

    feat: Create indexes on PR

--- scripts/create_index.py
+++ scripts/create_index.py
@@ -5,33 +5,42 @@ from os import listdir
 # Load the schema
 schema = load(open('server.schema.json'))
 
-# Create an empty array to store the index
-index = []
+def get_valid_servers():
+  validated_servers = []
 
-# Read the files in the directory
-for server in listdir('servers'):
-    print('Validating %s...' % server, end = ' ')
+  # Read the files in the directory
+  for server in listdir('servers'):
+      print('Validating %s...' % server, end = ' ')
 
-    # Open the file and load the JSON
-    with open('servers/%s' % server) as f:
-      try:
-        serverJson = load(f)
+      # Open the file and load the JSON
+      with open('servers/%s' % server) as f:
+        try:
+          serverJson = load(f)
 
-        # Validate the JSON
-        validate(serverJson, schema)
+          # Validate the JSON
+          validate(serverJson, schema)
 
-        # Add the server to the index
-        index.append(serverJson)
+          # Add the server to the index
+          validated_servers.append(serverJson)
 
-        print('OK')
-      except Exception as e:
-        print('FAILED')
+          print('OK')
+        except Exception as e:
+          print('FAILED')
+          print(e)
 
-# Save the index
-print('Saving index...', end = ' ')
-try:
-  with open('index.json', 'w') as f:
-    dump(index, f)
-  print('OK')
-except Exception as e:
-  print('FAILED')
+          exit(1)
+  
+  return validated_servers
+
+if __name__ == '__main__':
+  # Save the index
+  print('Saving index...', end = ' ')
+  try:
+    with open('index.json', 'w') as f:
+      dump(get_valid_servers(), f)
+    print('OK')
+  except Exception as e:
+    print('FAILED')
+    print(e)
+
+    exit(1)
